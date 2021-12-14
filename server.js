ca376a81c4d4039e11cd49b48c59277f62b4e2fa
fosabdul <um0364nu@go.minnstate.edu>
2021-12-14 13:59:00 -0600

    errorHandler

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 let express = require('express')
 let routes = require('./routes/facts.js')
+let path = require('path')
 
 let app = express()
 
+app.use(express.static(path.join(__dirname, 'content')))
+
 app.use('/api', routes)
 
-let server = app.listen(process.env.PORT || 3000, function() {
+// error handlers
+app.use(function(req, res, next){
+    res.status(404).send('Not found')
+})
+
+app.use(function(err, req, res, next){ 
+    console.error('Request to ' + req.originalUrl + ' errored because\n', err)
+    res.status(500).send('Server error')
+})
+
+let server = app.listen(process.env.PORT || 5000, function() {
     console.log('app running on port', server.address().port)
 })
